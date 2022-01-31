be84309fd08bd88cbd90769ffc20b66730ace4ea
yuhandan <2300936182@qq.com>
2022-01-31 14:14:47 +0800

    add Book model

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    book_name = models.CharField(max_length=128)
+    add_time = models.DateTimeField(auto_now_add=True)
+    def __unicode__(self):
+        return self.book_name
